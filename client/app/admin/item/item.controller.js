edddf11013330bde0b79f84178cc2028d29a0131
Paul Chhay <paulchhay@Pauls-MacBook-Pro.local>
2015-10-03 15:23:12 -0400

    admin create item frontend

--- client/app/admin/item/item.controller.js
+++ client/app/admin/item/item.controller.js
@@ -1,12 +1,36 @@
 'use strict';
 
 angular.module('blinkUrbanApp')
-  .controller('ItemCtrl', function ($scope) {
-  	$scope.item = {};
-  	
-    $scope.addItem = function(form){
-      if(form.$valid) {
+  .controller('ItemCtrl', function ($scope, $http, socket) {
 
+  	$scope.products = [];
+  	$scope.item = {};
+  	$scope.categories = ["Top", "Bottom", "Accessories"];
+
+  	$http.get('/api/items').success(function(products) {
+      $scope.products = products;
+      socket.syncUpdates('item', $scope.products);
+    });
+  	
+    $scope.addItem = function(){
+   	  if($scope.item === '') {
+      	return;
       }
+
+      $http.post('/api/items', $scope.item);
+      $scope.item = {};
+      $scope.form.$setUntouched()
+      //force refresh
+      location.reload();
     };
+
+    $scope.deleteItem = function(item) {
+      $http.delete('/api/items/' + item._id);
+      //force refresh
+      location.reload();
+    };
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('Item');
+    });
+
   });

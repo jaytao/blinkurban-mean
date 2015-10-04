dd4d0610c7b9c0a4f276d29ca033d03cee204f44
Paul Chhay <paulchhay@Pauls-MBP.home>
2015-10-03 22:08:32 -0400

    moved create item form to a modal

--- client/app/admin/item/item.controller.js
+++ client/app/admin/item/item.controller.js
@@ -1,28 +1,52 @@
 'use strict';
 
 angular.module('blinkUrbanApp')
-  .controller('ItemCtrl', function ($scope, $http, socket) {
+  .controller('ItemCtrl', function ($scope, $http, socket, $modal) {
 
   	$scope.products = [];
-  	$scope.item = {};
-  	$scope.categories = ["Top", "Bottom", "Accessories"];
-
   	$http.get('/api/items').success(function(products) {
       $scope.products = products;
       socket.syncUpdates('item', $scope.products);
     });
-  	
-    $scope.addItem = function(){
-   	  if($scope.item === '') {
-      	return;
-      }
 
-      $http.post('/api/items', $scope.item);
-      $scope.item = {};
-      $scope.form.$setUntouched()
-      //force refresh
-      location.reload();
-    };
+    $scope.createItemModal = function(){
+    	$modal.open({
+    		templateUrl: "app/admin/item/item.create.html",
+    		size: "lg",
+    		controller: function($scope, $modalInstance){
+    			$scope.item = {};
+			  	$scope.item.metrics = [];
+			  	$scope.metric = {};
+    			$scope.categories = ["Top", "Bottom", "Accessories"];
+			  	$scope.colors = ["Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Violet", 
+			  	"Purple", "Magenta", "Pink", "Brown", "White", "Gray", "Black"];
+			  	$scope.sizes = ["XXS","XS","S","M","L","XL", "XXL"];
+
+			  	$scope.addMetric = function(){
+			    	if($scope.metric !== "" && typeof $scope.metric.color !== 'undefined'
+			    		&& typeof $scope.metric.size !== 'undefined' && typeof $scope.metric.count !== 'undefined'){
+			    		$scope.item.metrics.push($scope.metric);
+			    		$scope.metric = {};
+			    	}
+			    };
+			    $scope.removeMetric = function(metric){
+			    	var index = $scope.item.metrics.indexOf(metric);
+			    	$scope.item.metrics.splice(index,1);
+			    };
+			    $scope.addItem = function(){
+			      $http.post('/api/items', $scope.item);
+			      $scope.item = {};
+			      //force refresh
+			      location.reload();
+			    };
+			    $scope.cancel = function(){
+			    	$modalInstance.dismiss('cancel');
+			    };
+
+    		}
+    	});
+    }
+  	
 
     $scope.deleteItem = function(item) {
       $http.delete('/api/items/' + item._id);

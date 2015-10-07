e5d2c19aba4f30ecf4a1bdb3758cd2961bd734a2
Paul Chhay <paulchhay@Pauls-MBP.home>
2015-10-06 22:49:02 -0400

    update function

--- client/app/admin/item/item.controller.js
+++ client/app/admin/item/item.controller.js
@@ -3,24 +3,33 @@
 angular.module('blinkUrbanApp')
   .controller('ItemCtrl', function ($scope, $http, socket, $modal) {
 
+    //TODO: make socket io work 
+
   	$scope.products = [];
   	$http.get('/api/items').success(function(products) {
       $scope.products = products;
       socket.syncUpdates('item', $scope.products);
     });
 
-    $scope.createItemModal = function(){
+    // two differnt mode fo this modal 'Create', 'Update'
+    $scope.openItemModal = function(mode, item){
     	$modal.open({
     		templateUrl: "app/admin/item/item.create.html",
     		size: "lg",
     		controller: function($scope, $modalInstance){
+
+          $scope.mode = mode;
     			$scope.item = {};
 			  	$scope.item.metrics = [];
 			  	$scope.metric = {};
-    			$scope.categories = ["Top", "Bottom", "Accessories"];
+    			$scope.categories = ["top", "bottom", "accessories"];
 			  	$scope.colors = ["Red", "Orange", "Yellow", "Green", "Cyan", "Blue", "Violet", 
 			  	"Purple", "Magenta", "Pink", "Brown", "White", "Gray", "Black"];
 			  	$scope.sizes = ["XXS","XS","S","M","L","XL", "XXL"];
+
+          if(mode === 'Update'){
+            $scope.item = item;
+          }
 
 			  	$scope.addMetric = function(){
 			    	if($scope.metric !== "" && typeof $scope.metric.colorname !== 'undefined' && typeof $scope.metric.colorhex !== 'undefined'
@@ -33,12 +42,18 @@ angular.module('blinkUrbanApp')
 			    	var index = $scope.item.metrics.indexOf(metric);
 			    	$scope.item.metrics.splice(index,1);
 			    };
-			    $scope.addItem = function(){
-			      $http.post('/api/items', $scope.item);
-			      $scope.item = {};
+
+			    $scope.submit = function(){
+            if($scope.mode === 'Create'){
+              $http.post('/api/items', $scope.item);
+            }else if($scope.mode === 'Update'){
+              $http.put('/api/items/' + $scope.item._id, $scope.item);
+            }
+			      
 			      //force refresh
 			      location.reload();
 			    };
+
 			    $scope.cancel = function(){
 			    	$modalInstance.dismiss('cancel');
 			    };
@@ -47,7 +62,6 @@ angular.module('blinkUrbanApp')
     	});
     }
   	
-
     $scope.deleteItem = function(item) {
       $http.delete('/api/items/' + item._id);
       //force refresh

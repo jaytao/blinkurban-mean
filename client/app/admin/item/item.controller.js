9ed45f9ef8ef4af5308e52a9dd796234cf862bf2
Paul Chhay <sophal.chhay@gmail.com>
2015-11-07 14:26:23 -0500

    add socket connection to admin/items

--- client/app/admin/item/item.controller.js
+++ client/app/admin/item/item.controller.js
@@ -84,9 +84,7 @@ angular.module('blinkUrbanApp')
             }else if($scope.mode === 'Update'){
               $http.put('/api/items/' + $scope.item._id, $scope.item);
             }
-			      
-			      //force refresh
-			      location.reload();
+            $scope.cancel();
 			    };
 
 			    $scope.cancel = function(){
@@ -117,11 +115,10 @@ angular.module('blinkUrbanApp')
   	
     $scope.deleteItem = function(item) {
       $http.delete('/api/items/' + item._id);
-      //force refresh
-      location.reload();
     };
     $scope.$on('$destroy', function () {
       socket.unsyncUpdates('Item');
+      socket.unsyncUpdates('Color');
     });
 
   });

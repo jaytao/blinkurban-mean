90b2d90033f1858f530f7243c3b94af5b7562a90
Paul Chhay <sophal.chhay@gmail.com>
2016-01-31 14:35:55 -0500

    coming back from vacation

--- client/app/apparel/apparel.controller.js
+++ client/app/apparel/apparel.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('blinkUrbanApp')
-  .controller('ApparelCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ApparelCtrl', function ($scope, $http, socket) {
+    $scope.categories = [];
+    $http.get('/api/categories').success(function(categories) {
+      $scope.categories = $scope.categories.concat(categories);
+      socket.syncUpdates('category', $scope.categories);
+    });
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('Category');
+    });
   });
--- server/api/category/category.model.js
+++ server/api/category/category.model.js
@@ -4,8 +4,8 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var CategorySchema = new Schema({
-  name: { type: String, maxlength: [128, "Category name cannot exceed {MAXLENGTH} characters"], required: "Category name is required"},
-  types: [{ type: String, maxlength: [128, "Type cannot exceed {MAXLENGTH} characters"]}],
+  name: { type: String, lowercase: true, maxlength: [128, "Category name cannot exceed {MAXLENGTH} characters"], required: "Category name is required"},
+  types: [{ type: String, lowercase: true, maxlength: [128, "Type cannot exceed {MAXLENGTH} characters"]}],
   active: Boolean
 });
 

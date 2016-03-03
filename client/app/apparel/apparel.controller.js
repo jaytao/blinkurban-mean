89f321a76f9cbf268434e1336dc22cc328817893
Paul Chhay <sophal.chhay@gmail.com>
2016-03-03 12:51:53 -0500

    added caveman filering system. needs an upgrade later

--- client/app/apparel/apparel.controller.js
+++ client/app/apparel/apparel.controller.js
@@ -18,8 +18,29 @@ angular.module('blinkUrbanApp')
     //TODO implement socket for items
     $scope.results = [];
     $scope.category = $stateParams.category;
+    $scope.filters = {
+      colors: [],
+      sizes: []
+    };
+
     $http.get('/api/items/get?category=' + $scope.category).success(function(data) {
       $scope.results = data;
+
+      //get all unique colors and sizes available and add it to the filter list
+      var tempColor = [];
+      data.forEach(function(item){
+        tempColor = _.merge(tempColor, _.pluck(item.metrics, 'colorId'));
+        item.metrics.forEach(function(metric){
+          $scope.filters.sizes.push({size: metric.size});
+        });
+      });
+      tempColor.forEach(function(item){
+        $http.get('/api/colors/'+item).success(function(obj){
+          $scope.filters.colors.push(obj);
+        });
+      });
+      $scope.filters.sizes = _.uniq($scope.filters.sizes, 'size');
+
     });
 
     //TODO this is duplicate from search controller
@@ -27,6 +48,41 @@ angular.module('blinkUrbanApp')
       return _.uniq(metrics, function(metric){
         return metric.colorId;
       });
+    };
+
+    $scope.show = function(color, size){
+      
+      var colorSelected = false;
+      var colorMatch = false;
+      $scope.filters.colors.forEach(function(filter){
+        if(filter.selected){
+          colorSelected = true;
+          if(filter._id === color){
+            colorMatch = true;
+          }
+        }
+      });
+
+      var sizeSelected = false;
+      var sizeMatch = false;
+      $scope.filters.sizes.forEach(function(filter){
+        if(filter.selected){
+          sizeSelected = true;
+          if(filter.size === size){
+            sizeMatch = true;
+          }
+        }
+      });
+
+      if(colorSelected && sizeSelected){
+        return colorMatch && sizeMatch;
+      }else if(colorSelected){
+        return colorMatch;
+      }else if(sizeSelected){
+        return sizeMatch;
+      }
+
+      return true;
     };
 
   });

a01bac64ebc82be2f6e47fe5b6cc65bcf68936be
Dee <dtnifti@gmail.com>
2016-04-28 20:53:23 -0400

    clean navbar

--- client/app/cart/cart.controller.js
+++ client/app/cart/cart.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('blinkUrbanApp')
-  .controller('CartCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('CartCtrl', function ($scope, $http, Auth) {
+    
+    $scope.cartItems = [];
+    var userId = Auth.getCurrentUser()._id;
+    $http.get("/api/cart").then(function(result){
+    	$scope.cartItems.push(result);
+    });
+
+    $http.post("").then(function(){
+
+    });
+    
+
   });

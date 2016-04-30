0e5958c9ec7d578420c756da6dda280ee554f355
Jeff <jeffs7tao@gmail.com>
2016-04-30 11:53:38 -0400

    merge

--- client/app/cart/cart.controller.js
+++ client/app/cart/cart.controller.js
@@ -1,26 +1,11 @@
 'use strict';
 
 angular.module('blinkUrbanApp')
-<<<<<<< HEAD
   .controller('CartCtrl', function ($scope,$http) {
     $scope.message = 'Hello';
     $scope.items = [];
     $http.get('/api/cart/56ec918c72723c2b4c183e71').success(function(cart){
         $scope.items = cart.items;
     });
-=======
-  .controller('CartCtrl', function ($scope, $http, Auth) {
-    
-    $scope.cartItems = [];
-    var userId = Auth.getCurrentUser()._id;
-    $http.get("/api/cart").then(function(result){
-    	$scope.cartItems.push(result);
-    });
 
-    $http.post("").then(function(){
-
-    });
-    
-
->>>>>>> a01bac64ebc82be2f6e47fe5b6cc65bcf68936be
   });

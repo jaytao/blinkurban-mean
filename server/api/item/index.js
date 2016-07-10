4fea277ac284c0d069a2128fd2db4d5d40bb98d4
Jeff <jeffs7tao@gmail.com>
2016-07-10 17:46:08 -0400

    admin items call returns basecost

--- server/api/item/index.js
+++ server/api/item/index.js
@@ -9,6 +9,7 @@ var router = express.Router();
 router.get("/search", controller.search);
 router.get("/get", controller.get);
 router.get('/', controller.index);
+router.get('/admin', auth.hasRole('admim'), controller.index);
 router.get('/:id', controller.show);
 router.post('/', auth.hasRole('admin'), controller.create);
 router.post('/:id/add', auth.hasRole('admin'), controller.add);
